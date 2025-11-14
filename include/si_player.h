07562b6ea91de92536d1861b574b67de7856d478
Victor Mankowski <victor.mankowski@gmail.com>
2025-11-14 22:23:04 +0100

    updating fire animation logic

--- include/si_player.h
+++ include/si_player.h
@@ -1,10 +1,12 @@
 #ifndef SI_PLAYER_H
 #define SI_PLAYER_H
 
+#include "bn_span.h"
 #include "bn_deque.h"
 #include "bn_optional.h"
 #include "bn_sprite_ptr.h"
 #include "bn_fixed_point.h"
+#include "bn_sprite_animate_actions.h"
 
 #include "bn_sprite_items_spaceship.h"
 
@@ -12,18 +14,18 @@
 namespace si {
     class Player {
         private:
+            // Spaceship attributes
             bn::sprite_ptr _spaceship_sprite = bn::sprite_items::spaceship.create_sprite(0, 70);
             bn::fixed_point _spaceship_position = bn::fixed_point(0, 70);
             bn::fixed _spaceship_speed = 1;
-            bn::deque<bn::optional<bn::sprite_ptr>, 32> _bullets;
-            const bn::fixed _bullet_speed = 3; 
 
-            bool _fire = false;
+            // Bullets attributes
+            bn::deque<bn::optional<bn::sprite_ptr>, 32> _bullets;
+            const bn::fixed _bullet_speed = -3;
+
+            // Fire animation attributes
             bn::optional<bn::sprite_ptr> _fire_sprite;
-            int _fire_sprite_animation_key = 0;
-            const bn::fixed _fire_sprite_animation_max_key = 3;
-            bn::fixed _animation_timer = 0;
-            const bn::fixed _animation_cooldown = 2;
+            bn::optional<bn::sprite_animate_action<4>> _fire_action;
 
         public:
             Player();
--- src/si_player.cpp
+++ src/si_player.cpp
@@ -3,14 +3,16 @@
 #include "bn_log.h"
 #include "bn_keypad.h"
 #include "bn_sprite_tiles_ptr.h"
+#include "bn_sprite_animate_actions.h"
 
-#include "bn_sprite_items_ammo.h"
 #include "bn_sprite_items_fire.h"
+#include "bn_sprite_items_ammo.h"
 
 namespace si {
     Player::Player() {}
 
     void Player::update() {
+        // Controller
         if (bn::keypad::left_held() && _spaceship_sprite.x() >= -111) {
             _spaceship_sprite.set_x(_spaceship_sprite.x() - _spaceship_speed);
             _spaceship_sprite.set_tiles(bn::sprite_items::spaceship.tiles_item().create_tiles(1));
@@ -22,40 +24,42 @@ namespace si {
         else {
             _spaceship_sprite.set_tiles(bn::sprite_items::spaceship.tiles_item().create_tiles(0));
         }
-        
+
+        // Bullet behaviour
         if (bn::keypad::a_pressed()) {
-            if (_bullets.full()) {
+            // If the bullet buffer is full, pop the first element
+            if (_bullets.full())
                 _bullets.pop_front();
-            } 
+
+            // Calculate where the bullet should appear and push the sprite element at the back of the queue
             _bullets.push_back(bn::sprite_items::ammo.create_sprite(_spaceship_sprite.x() - 0.5, _spaceship_sprite.y() - 7.5));
             _bullets.back().get()->set_scale(0.5);
 
+            // Instantiate the player fire sprite and play the animation
             _fire_sprite = bn::sprite_items::fire.create_sprite(_spaceship_sprite.x(), _spaceship_sprite.y() - 2);
-            _fire_sprite_animation_key = 1;
+            _fire_action = bn::create_sprite_animate_action_once(
+                _fire_sprite.value(), 3, bn::sprite_items::fire.tiles_item(), 0, 1, 2, 3);
         }
-        
+
         for (auto it = _bullets.begin(); it != _bullets.end(); it++) {
-            it->get()->set_y(it->get()->y() - _bullet_speed);
+            it->get()->set_y(it->get()->y() + _bullet_speed);
 
             if (it->get()->y() <= -80) {
                 _bullets.erase(it);
                 break;
             }
+            // it->get()->update();
+        }
+
+        // Fire animation update logic
+        if (_fire_action.has_value() && _fire_action.get()->done()) {
+            _fire_sprite.reset();
         }
 
         if (_fire_sprite.has_value()) {
-            if (_animation_timer == _animation_cooldown) {
-                _fire_sprite.get()->set_tiles(bn::sprite_items::fire.tiles_item().create_tiles(_fire_sprite_animation_key));
-                if (_fire_sprite_animation_key < _fire_sprite_animation_max_key)
-                    _fire_sprite_animation_key+= 1;
-                else {
-                    _fire_sprite_animation_key = 0;
-                    _fire_sprite.reset();
-                }
-                _animation_timer = 0;
-            } else {
-                _animation_timer+= 1;
-            }
+            _fire_action.get()->update();
+        } else {
+            _fire_action.reset();
         }
     }
 }
